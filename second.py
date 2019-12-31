11b32d3a83f7d6ffa9afc4d5128f25d0c532e728
jiangyoujia <18269584938@163.com>
2019-12-31 19:28:07 +0800

    第7行进行注释

--- second.py
+++ second.py
@@ -4,7 +4,7 @@ n=input("Please input a num:") #请输入整数
 
 print("{:->20}".format(eval(n)))
 
-random.seed(123)
+random.seed(123) #以123为种子生成随机数
 for i in range(10):
     print(random.randint(1,999),end=",")
 
