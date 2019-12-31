cbcbbb0547b8d53ede22fb6b99260ccc24f61263
test <test@email>
2019-12-31 20:43:37 +0800

    添加两个数相乘的结果

--- second.py
+++ second.py
@@ -1,3 +1,4 @@
+
 import random
 
 n=input("Please input a num:") #请输入整数
@@ -7,4 +8,14 @@ print("{:->20}".format(eval(n)))
 random.seed(123) #以123为种子生成随机数
 for i in range(10):
     print(random.randint(1,999),end=",")
+print()
 
+a=[[1,2,3],[4,5,6],[7,8,9]]
+b=[3,6,9]
+
+s=0
+
+for c in a:
+    for j in range(3):
+        s+=c[j]*b[j]
+print("总和为：",s)
